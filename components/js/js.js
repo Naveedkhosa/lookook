c8efafc5600846aabd18c4bb5f079a763061dc9f
Naveed Khosa <naveedullahkhosa@gmail.com>
2023-12-01 15:41:06 +0500

    initial

--- components/js/js.js
+++ components/js/js.js
@@ -1,13 +1,3 @@
-// onload  = start;
-
-// function start(){	
-// var i = 1;
-// function Move(){ 	
-// 	i = (i%4)+1; // 4 is the Number of image in slider
-// 	document.getElementById('i'+i).checked = true;
-// }
-// setInterval(Move,3000); //change img in 3 sec
-// }
 
 function openModal(modal)
 {
@@ -45,26 +35,3 @@ function openModall(modalId) {
 }
 
 
-
-//  document.getElementById("right_sidebr").addEventListener("click", function () {
-//     var roverlay = document.querySelector(".roverlay");
-//     roverlay.style.display = 'flex';
-
-//     // Trigger a reflow to restart the transition
-//     void roverlay.offsetWidth;
-
-//     roverlay.style.opacity = '1';
-//   });
-
-  // document.querySelector(".roverlay").addEventListener("click", function (e) {
-  //   if (e.target == e.currentTarget) {
-  //     var roverlay = document.querySelector(".roverlay");
-  //     roverlay.style.opacity = '0';
-
-  //     setTimeout(function () {
-  //       roverlay.style.display = 'none';
-  //     }, 500); // Adjust the delay to match the transition duration
-  //   } else {
-  //     // Add any other actions you want to perform when clicking inside the sidebar
-  //   }
-  // });
