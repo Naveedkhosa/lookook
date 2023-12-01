92b9b77faf00dba06609e8dc5a0e874286c1c1e9
Naveed Khosa <naveedullahkhosa@gmail.com>
2023-12-01 15:15:59 +0500

    initial

--- components/js/js.js
+++ components/js/js.js
@@ -1,13 +1,13 @@
-onload  = start;
+// onload  = start;
 
-function start(){	
-var i = 1;
-function Move(){ 	
-	i = (i%4)+1; // 4 is the Number of image in slider
-	document.getElementById('i'+i).checked = true;
-}
-setInterval(Move,3000); //change img in 3 sec
-}
+// function start(){	
+// var i = 1;
+// function Move(){ 	
+// 	i = (i%4)+1; // 4 is the Number of image in slider
+// 	document.getElementById('i'+i).checked = true;
+// }
+// setInterval(Move,3000); //change img in 3 sec
+// }
 
 function openModal(modal)
 {
@@ -23,26 +23,48 @@ function openModal(modal)
       }
     }
 }
- // this is side bar script
- document.getElementById("right_sidebr").addEventListener("click", function () {
-    var roverlay = document.querySelector(".roverlay");
-    roverlay.style.display = 'flex';
 
-    // Trigger a reflow to restart the transition
-    void roverlay.offsetWidth;
+function openModall(modalId) {
+  var modal = document.getElementById(modalId);
 
-    roverlay.style.opacity = '1';
-  });
+  // Toggle the 'active' class
+  modal.classList.toggle('active');
 
-  document.querySelector(".roverlay").addEventListener("click", function (e) {
-    if (e.target == e.currentTarget) {
-      var roverlay = document.querySelector(".roverlay");
-      roverlay.style.opacity = '0';
+  var span = document.querySelector("#" + modalId + " .close");
+  span.onclick = function () {
+    // Toggle the 'active' class when the close button is clicked
+    modal.classList.toggle('active');
+  }
 
-      setTimeout(function () {
-        roverlay.style.display = 'none';
-      }, 500); // Adjust the delay to match the transition duration
-    } else {
-      // Add any other actions you want to perform when clicking inside the sidebar
+  window.onclick = function (event) {
+    // Toggle the 'active' class when clicking outside the modal
+    if (event.target == modal) {
+      modal.classList.toggle('active');
     }
-  });
+  }
+}
+
+
+
+//  document.getElementById("right_sidebr").addEventListener("click", function () {
+//     var roverlay = document.querySelector(".roverlay");
+//     roverlay.style.display = 'flex';
+
+//     // Trigger a reflow to restart the transition
+//     void roverlay.offsetWidth;
+
+//     roverlay.style.opacity = '1';
+//   });
+
+  // document.querySelector(".roverlay").addEventListener("click", function (e) {
+  //   if (e.target == e.currentTarget) {
+  //     var roverlay = document.querySelector(".roverlay");
+  //     roverlay.style.opacity = '0';
+
+  //     setTimeout(function () {
+  //       roverlay.style.display = 'none';
+  //     }, 500); // Adjust the delay to match the transition duration
+  //   } else {
+  //     // Add any other actions you want to perform when clicking inside the sidebar
+  //   }
+  // });
