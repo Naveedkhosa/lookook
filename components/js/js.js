53d8a52a05624830883a86b8860632143eab5b23
Naveed Khosa <naveedullahkhosa@gmail.com>
2024-01-02 19:45:16 +0500

    frontend

--- components/js/js.js
+++ components/js/js.js
@@ -49,3 +49,23 @@ function confirmAction(modId) {
   alert("Confirmed for " + modId + "!");
   closePopup(modId); // Close the popup after confirming
 }
+
+
+// pop up 120 js start
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+// pop up 120 js end 
